0df2018a938ba79ae4913da9f9b53ee7148a2cc0
Karan Rawal <Manheaten@gmail.com>
2025-09-16 22:48:48 +0530

    Create  Target Sum Problem.cpp

---
+++ 16-09-2025/Target Sum Problem.cpp
@@ -0,0 +1,22 @@
+// Target Sum Problem Leetcode
+class Solution {
+public:
+int ways(vector<int>& nums, int target , int i , int n){
+
+     if (i == n) {
+            return target == 0 ? 1 : 0;
+        }
+
+    int pos= ways(nums, target-nums[i], i+1, n);
+    int neg= ways(nums, target+nums[i], i+1, n);
+
+    return pos+neg;
+
+
+}
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int index=0;
+        int n=nums.size();
+        return ways(nums , target , index , n);
+    }
+};
