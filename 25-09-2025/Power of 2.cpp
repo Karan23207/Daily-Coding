1d04f655ab6906cddfffa0e05e1fd0f92d9f4bd4
Karan Rawal <Manheaten@gmail.com>
2025-09-25 23:54:30 +0530

    Create Power of 2.cpp

---
+++ 25-09-2025/Power of 2.cpp
@@ -0,0 +1,13 @@
+// GFG PROBLEM
+class Solution {
+  public:
+    bool checkKthBit(int n, int k) {
+        //  code here
+        int ans=1<<k;
+        
+        if(n & ans)
+        return 1;
+        else
+        return 0;
+    }
+};
